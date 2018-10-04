ae24761080580c8546afd64fd04f7a22dfcee370
Iggy Grey <github@pmphillips.ca>
2018-10-04 10:16:18 -0700

    Added start_chain. Edited example.js

--- example.js
+++ example.js
@@ -1,9 +1,11 @@
 var Network = require('.');
 
+const platformOwner = '0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1'; //platform owner is accounts[0]
+
 const operator = Network.addOperator(
     '0xffcf8fdee72ac11b5c542428b35eef5769c409f0',
     'Mac the Operator',
-    '0xffcf8fdee72ac11b5c542428b35eef5769c409f0'
+    platformOwner
 );
 
 operator.then(function(res) {
