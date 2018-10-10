18d96c117c46b35109e92eb76812b5c604ef11a0
Iggy Grey <github@pmphillips.ca>
2018-10-10 15:29:04 -0700

    Changed to async functions. Updated example.js to show a successful crowdsale

--- example.js
+++ example.js
@@ -1,13 +1,69 @@
 var Network = require('.');
+var accounts = require("@mybit/chain/accounts.json");
 
-const platformOwner = '0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1'; //platform owner is accounts[0]
+const platformOwner = accounts[0];
+const operatorAddress = accounts[1];
 
-const operator = Network.addOperator(
-    '0xffcf8fdee72ac11b5c542428b35eef5769c409f0',
+var operatorID;
+var assetID;
+
+async function setOperator(){
+  var id = await Network.addOperator(
+    operatorAddress,
     'Mac the Operator',
     platformOwner
-);
+  );
 
-operator.then(function(res) {
-    console.log('Response', res)
-})
+  await Network.acceptEther(id, operatorAddress);
+
+  return id;
+}
+
+async function startCrowdsale(){
+  await Network.approveBurn(operatorAddress);
+
+  var id = await Network.createAsset({
+      assetURI: "CoffeeRun",
+      operatorID: operatorID,
+      fundingLength: 1000,
+      amountToRaise: 70000000000000000, //about $20 CAD
+      brokerPercent: 0,
+      broker: operatorAddress //operator is also broker
+  });
+
+  return id;
+}
+
+async function contribute(account, amount){
+  await Network.approveBurn(account);
+  await Network.fundAsset({
+      assetID: assetID,
+      amount: amount,
+      address: account
+  });
+}
+
+async function fundCoffee(){
+  //Setup operator (who will also be the broker)
+  operatorID = await setOperator();
+  console.log('Operator ID: ', operatorID);
+  //Start the crowdsale
+  var response = await startCrowdsale();
+  assetID = response._assetID;
+  console.log('Asset ID: ', assetID);
+  var tokenAddress = response._tokenAddress;
+  console.log('Token Address: ', tokenAddress);
+  //Check operator's funds before
+  console.log('Operator ether before: ', await web3.eth.getBalance(operatorAddress));
+  //Two users contribute
+  await contribute(accounts[3], 30000000000000000);
+  await contribute(accounts[4], 40000000000000000);
+  //Check operator's funds after
+  console.log('Operator ether after: ', await web3.eth.getBalance(operatorAddress));
+  token = await Network.dividendTokenETH(tokenAddress);
+  console.log('Operator assets: ', Number(await token.balanceOf(operatorAddress)));
+  console.log('Investor 1 assets: ', Number(await token.balanceOf(accounts[3])));
+  console.log('Investor 2 assets: ', Number(await token.balanceOf(accounts[4])));
+}
+
+fundCoffee();
--- index.js
+++ index.js
@@ -19,9 +19,9 @@ module.exports = (function (){
     // set the provider you want from Web3.providers
     web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
   }
-  
+
   //Setup contracts
-  //var mybitContract = contract(ContractArtifacts.MyBit());
+  var mybitContract = contract(ContractArtifacts.BurnableToken);
   var erc20BurnerContract = contract(ContractArtifacts.ERC20Burner);
   var databaseContract = contract(ContractArtifacts.Database);
   var contractManagerContract = contract(ContractArtifacts.ContractManager);
@@ -36,57 +36,115 @@ module.exports = (function (){
   var crowdsaleERC20Contract = contract(ContractArtifacts.CrowdsaleERC20);
   var crowdsaleGeneratorERC20Contract = contract(ContractArtifacts.CrowdsaleGeneratorERC20);
   var assetExchangeContract = contract(ContractArtifacts.AssetExchange);
+  var divTokenETHContract = contract(ContractArtifacts.DividendToken);
+  var divTokenERCContract = contract(ContractArtifacts.DividendTokenERC20);
 
   return {
-    // approveBurn: function(fromAddress){
-    //   return new Promise((resolve, reject) => {
-    //     var count = 0;
-    //     var amount = 1000000000000000000000000000000; //Some large amount 10^30
-    //     var burnerAddress = Chain.ERC20Burner();
-    //     mybitContract.at(Chain.MyBit()).then(function(instance){
-    //       return instance.approve(burnerAddress, amount, {from: fromAddress});
-    //     }).then(function(){
-    //       complete();
-    //     });
+    dividendTokenETH: async (tokenAddress) => {
+      return await divTokenETHContract.at(tokenAddress);
+    },
 
-    //     erc20BurnerContract.at(burnerAddress).then(function(instance){
-    //       return instance.givePermission({from: fromAddress});
-    //     }).then(function(){
-    //       complete();
-    //     });
+    dividendTokenERC20: async (tokenAddress) => {
+      return await divTokenERCContract.at(tokenAddress);
+    },
 
-    //     function complete(){
-    //       if(count == 1){
-    //         return resolve(true);
-    //       } else {
-    //         count++;
-    //       }
-    //     }
-    //   });
-    // },
+    approveBurn: async (fromAddress) => {
+      var count = 0;
+      var amount = 1000000000000000000000000000000; //Some large amount 10^30
+      var burnerAddress = Chain.ERC20Burner();
+      tokenInstance = await mybitContract.at(Chain.MyBit());
+      await tokenInstance.approve(burnerAddress, amount, {from: fromAddress});
+      burnerInstance = await erc20BurnerContract.at(burnerAddress);
+      await burnerInstance.givePermission({from: fromAddress});
+      return true;
 
-    addOperator: function(account, name, owner){
+      /*
+      return new Promise((resolve, reject) => {
+        var count = 0;
+        var amount = 1000000000000000000000000000000; //Some large amount 10^30
+        var burnerAddress = Chain.ERC20Burner();
+        mybitContract.at(Chain.MyBit()).then(function(instance){
+        return instance.approve(burnerAddress, amount, {from: fromAddress});
+        }).then(function(){
+          complete();
+        });
+
+        erc20BurnerContract.at(burnerAddress).then(function(instance){
+        return instance.givePermission({from: fromAddress});
+        }).then(function(){
+          complete();
+        });
+
+        function complete(){
+          if(count == 1){
+            return resolve(true);
+          } else {
+            count++;
+          }
+        }
+      });
+      */
+    },
+
+    addOperator: async (account, name, owner) => {
+      instance = await operatorsContract.at(Chain.Operators());
+      tx = await instance.registerOperator(account, name, {from: owner});
+      return tx.logs[0].args._operatorID;
+      /*
       return new Promise((resolve, reject) => {
         operatorsContract.at(Chain.Operators()).then(function(instance){
           return instance.registerOperator(account, name, {from: owner});
         }).then(function(tx){
           var operatorID = tx.logs[0].args._operatorID
           return resolve(operatorID);
+        }).catch(e => {
+          return reject(e);
         });
       });
+      */
     },
 
-    acceptEther: function(id, address){
+    acceptEther: async (id, operatorAddress) => {
+      instance = await operatorsContract.at(Chain.Operators());
+      await instance.acceptEther(id, true, {from: operatorAddress});
+      return true;
+      /*
       return new Promise((resolve, reject) => {
         operatorsContract.at(Chain.Operators()).then(function(instance){
-          return instance.acceptEther(id, true, {from: address});
+          return instance.acceptEther(id, true, {from: operatorAddress});
         }).then(function(tx){
           return resolve(true);
         });
       });
+      */
     },
 
-    createAsset: function(object){
+    acceptERC20Token: async (id, tokenAddress, operatorAddress) => {
+      instance = await operatorsContract.at(Chain.Operators());
+      await instance.acceptERC20Token(id, tokenAddress, true, {from: operatorAddress});
+      return true;
+      /*
+      return new Promise((resolve, reject) => {
+        operatorsContract.at(Chain.Operators()).then(function(instance){
+          return instance.acceptERC20Token(id, tokenAddress, true, {from: operatorAddress});
+        }).then(function(tx){
+          return resolve(true);
+        });
+      });
+      */
+    },
+
+    createAsset: async (object) => {
+      if(object.fundingToken){
+        instance = await crowdsaleGeneratorERC20Contract.at(Chain.CrowdsaleGeneratorERC20());
+        tx = await instance.createAssetOrderERC20(object.assetURI, object.operatorID, object.fundingLength, object.amountToRaise, object.brokerFee, object.fundingToken, {from: object.broker, gas:2300000});
+        return tx.logs[0].args;
+      } else {
+        instance = await crowdsaleGeneratorETHContract.at(Chain.CrowdsaleGeneratorETH());
+        tx = await instance.createAssetOrderETH(object.assetURI, object.operatorID, object.fundingLength, object.amountToRaise, object.brokerPercent, {from: object.broker, gas:2300000});
+        return tx.logs[0].args;
+      }
+      /*
       return new Promise((resolve, reject) => {
         if(object.fundingToken){
           crowdsaleGeneratorERC20Contract.at(Chain.CrowdsaleGeneratorERC20())
@@ -98,33 +156,45 @@ module.exports = (function (){
         } else {
           crowdsaleGeneratorETHContract.at(Chain.CrowdsaleGeneratorETH())
           .then(function(instance){
-            return instance.createAssetOrderETH(object.assetURI, object.operatorID, object.fundingLength, object.amountToRaise, object.brokerFee, {from: object.broker, gas:2300000});
+            return instance.createAssetOrderETH(object.assetURI, object.operatorID, object.fundingLength, object.amountToRaise, object.brokerPercent, {from: object.broker, gas:2300000});
           }).then(function(tx){
             return resolve(tx.logs[0].args._assetID);
           });
         }
       });
+      */
     },
 
-    fundAsset: function(object){
+    fundAsset: async (object) => {
+      if(object.fundingToken){
+        instance = await crowdsaleERC20Contract.at(Chain.CrowdsaleERC20());
+        tx = await instance.buyAssetOrderERC20(object.assetID, object.amount, {from: object.address, gas:2300000});
+        return tx.tx;
+      } else {
+        instance = await crowdsaleETHContract.at(Chain.CrowdsaleETH());
+        tx = await instance.buyAssetOrderETH(object.assetID, {from: object.address, value: object.amount, gas:2300000});
+        return tx.tx;
+      }
+      /*
       return new Promise((resolve, reject) => {
         if(object.fundingToken){
           crowdsaleERC20Contract.at(Chain.CrowdsaleERC20())
           .then(function(instance){
-            return instance.buyAssetOrder(object.assetID, object.amount, {from: object.address, gas:2300000});
+            return instance.buyAssetOrderERC20(object.assetID, object.amount, {from: object.address, gas:2300000});
           }).then(function(tx){
             return resolve(tx.tx);
           });
         } else {
           crowdsaleETHContract.at(Chain.CrowdsaleETH())
           .then(function(instance){
-            return instance.buyAssetOrder(object.assetID, {from: object.address, value: object.amount, gas:2300000});
+            return instance.buyAssetOrderETH(object.assetID, {from: object.address, value: object.amount, gas:2300000});
           }).then(function(tx){
             console.log(tx);
             return resolve(tx.tx);
           });
         }
       });
+      */
     }
   }
 })();
