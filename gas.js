7fc3e32493f7a6a842163ddb2db3a2b9bcf99ae7
Peter Phillips <github@pmphillips.ca>
2019-04-17 22:53:27 -0700

    Added gas.js

---
+++ gas.js
@@ -0,0 +1,18 @@
+module.exports = {
+  addOperator: '110000',
+  acceptEther: '55000',
+  acceptERC20Token: '55000',
+  approve: '55000',
+  createAssetOrderETH: '1900000',
+  createAssetOrderERC20: '1900000',
+  createTradeableAsset: '1450000',
+  createAsset: '525000',
+  createDividendToken: '1750000',
+  createERC20Token: '1750000',
+  buyAssetOrderETH: '350000',
+  buyAssetOrderERC20: '350000',
+  payoutETH: '1000000',
+  payoutERC20: '1000000',
+  issueDividendsETH: '70000',
+  issueDividendsERC20: '110000'
+}
